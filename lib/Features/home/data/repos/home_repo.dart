25ee20705c294896ca46a89b5456dd5f484d4e8f
AymnBnShaban <aymnm1957@gmail.com>
2025-06-24 23:27:36 +0300

    understand what is Either from dartz

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,5 +1,9 @@
-abstract class HomeRepo {
-  fetchBestSellerBooks();
+import 'package:dartz/dartz.dart';
+import 'package:scripto_books/Features/home/data/models/books_model.dart';
+import 'package:scripto_books/core/errors/failures.dart';
 
-  fetchFeaturedBooks();
+abstract class HomeRepo {
+  Future<Either<Failures, List<BooksModel>>> fetchBestSellerBooks();
+
+  Future<Either<Failures, List<BooksModel>>> fetchFeaturedBooks();
 }
