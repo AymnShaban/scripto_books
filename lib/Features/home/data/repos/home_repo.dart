29aa919acf9df09092c27053a8fa728aaf5764e2
AymnBnShaban <aymnm1957@gmail.com>
2025-06-25 03:03:22 +0300

    implement fetch resent books

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -3,7 +3,7 @@ import 'package:scripto_books/Features/home/data/models/books_model.dart';
 import 'package:scripto_books/core/errors/failures.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failures, List<BooksModel>>> fetchBestSellerBooks();
+  Future<Either<Failures, List<BooksModel>>> fetchRecentBooks();
 
   Future<Either<Failures, List<BooksModel>>> fetchFeaturedBooks();
 }
--- lib/Features/home/data/repos/home_repo_implement.dart
+++ lib/Features/home/data/repos/home_repo_implement.dart
@@ -2,17 +2,29 @@ import 'package:dartz/dartz.dart';
 import 'package:scripto_books/Features/home/data/models/books_model.dart';
 import 'package:scripto_books/Features/home/data/repos/home_repo.dart';
 import 'package:scripto_books/core/errors/failures.dart';
+import 'package:scripto_books/core/utils/api_service.dart';
 
-class HomeRepoImpl implements HomeRepo{
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+
   @override
-  Future<Either<Failures, List<BooksModel>>> fetchBestSellerBooks() {
-
-    throw UnimplementedError();
+  Future<Either<Failures, List<BooksModel>>> fetchRecentBooks() async {
+    try {
+      var data = await apiService.get(endPoint: 'recent');
+      List<BooksModel> books = [];
+      for (var item in data['books']) {
+        books.add(BooksModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
   Future<Either<Failures, List<BooksModel>>> fetchFeaturedBooks() {
-
     throw UnimplementedError();
   }
 }
